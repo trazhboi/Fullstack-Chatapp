731461b5d4a76723f4cc650a56211edc83aefbd2
Kishalay <k.kashyap.x@gmail.com>
2026-03-24 01:38:23 +0530

    fix production API and socket URLs

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chatapp-production-f44b.up.railway.app/api",
   withCredentials: true,
 });
