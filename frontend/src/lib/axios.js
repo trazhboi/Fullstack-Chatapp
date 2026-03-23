818a4e13c59239ba0bafde88eb43aa3da5307561
Kishalay <k.kashyap.x@gmail.com>
2026-03-24 02:44:51 +0530

    fix missing /api in production baseURL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chatapp-production-c34b.up.railway.app",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chatapp-production-c34b.up.railway.app/api",
   withCredentials: true,
 });
