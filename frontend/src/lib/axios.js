30c175f9d96b3246699ef5c8cfd37c6695eb623a
Kishalay <k.kashyap.x@gmail.com>
2026-03-24 02:37:01 +0530

    update railway URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chatapp-production-f44b.up.railway.app/api",
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://fullstack-chatapp-production-c34b.up.railway.app",
   withCredentials: true,
 });
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://fullstack-chatapp-production-f44b.up.railway.app";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "https://fullstack-chatapp-production-c34b.up.railway.app";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
